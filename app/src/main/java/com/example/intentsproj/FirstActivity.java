ccbdddaa63c71aef1e910d7acce15b2b049fa358
Abishek <abishaj1998@gmail.com>
2020-03-27 14:07:51 +0530

    T2 Final Commit

--- app/src/main/java/com/example/intentsproj/FirstActivity.java
+++ app/src/main/java/com/example/intentsproj/FirstActivity.java
@@ -15,37 +15,33 @@ import android.widget.Toast;
 
 public class FirstActivity extends AppCompatActivity {
 
-    public static final String EXTRA_MESSAGE = "com.example.intentsproj";
-    EditText editText1,editText2;
-    Button button;
+    EditText number1,number2;
+    Button btn_ok;
+
+    String num1;
+    String num2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
 
-        editText1 = (EditText)findViewById(R.id.editText1);
-        editText2 = (EditText)findViewById(R.id.editText2);
-        button = (Button) findViewById(R.id.button);
+        number1 = (EditText)findViewById(R.id.id_input1);
+        number2 = (EditText)findViewById(R.id.id_input2);
+        btn_ok = (Button) findViewById(R.id.btn_ok);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        btn_ok.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent (FirstActivity.this,SecondActivity.class);
+                intent.putExtra("NUMBER1",number1.getText().toString());
+                intent.putExtra("NUMBER2",number2.getText().toString());
+
                 Context context = getApplicationContext();
                 CharSequence message = "Toast Successfull";
                 int duration = Toast.LENGTH_SHORT;
                 Toast toast = Toast.makeText(context,message,duration);
-                toast.setGravity(Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL,0,0);
                 toast.show();
-
-                Intent intent = new Intent (FirstActivity.this,SecondActivity.class);
-                editText1 = (EditText)findViewById(R.id.editText1);
-                String message2 = editText1.getText().toString();
-                intent.putExtra(EXTRA_MESSAGE,message2);
-
-                editText2 = (EditText)findViewById(R.id.editText2);
-                String message3 = editText2.getText().toString();
-                intent.putExtra(EXTRA_MESSAGE,message3);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/intentsproj/MainActivity.java
+++ app/src/main/java/com/example/intentsproj/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.intentsproj;
 
+import android.content.Intent;
 import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
 import android.text.Layout;
@@ -7,27 +8,37 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button nextPage;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(final Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        LayoutInflater li = getLayoutInflater();
+        LayoutInflater layoutInflater = getLayoutInflater();
+        View layout = layoutInflater.inflate(R.layout.customtoast,(ViewGroup)findViewById(R.id.custom_toast_layout));
 
-        View layout = li.inflate(R.layout.customtoast,(ViewGroup)
-        findViewById(R.id.custom_toast_layout));
-
+        nextPage = (Button) findViewById(R.id.id_btn_calculator);
         Toast toast = new Toast(getApplicationContext());
         toast.setDuration(Toast.LENGTH_SHORT);
         toast.setGravity(Gravity.CENTER_VERTICAL,0,0);
         toast.setView(layout);
         toast.show();
+
+        nextPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,FirstActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
